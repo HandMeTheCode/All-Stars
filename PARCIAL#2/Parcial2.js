23363d5add47c9bca0b0f3906459434c65e07bae
cristian-andrade <cristian.andrade@utp.ac.pa>
2022-11-07 23:56:53 -0500

    agregando PARCIAL#2

---
+++ PARCIAL#2/Parcial2.js
@@ -0,0 +1,14 @@
+//visualizacion
+//creacion de la piramide
+//crear un array o varios arrays el cual tenga una cantidad limitada de variantes que se puedan introducir
+//cada fila del array solo podra introducir una variante paralela a la cantidad de filas que se hayan que generar
+//crear loop en siguiente logistica
+//primer fila comenzara con capacidad para 1 variante
+//caso de que al introducir mas variantes que la cantidad permitida, crear nueva fila + 1 variante
+
+//visualizacion
+//encontrar ruta mas pesada
+//primero encontrar variante mas pesada por fila
+//leer variante y compara con otras variantes si se encuentran
+//leer variante a mientras que variante a sea mayor al resto de variantes en fila, añadir color rojo
+//si se introduce "," leerla como null
